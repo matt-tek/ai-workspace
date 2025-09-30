9700b5b1063e5da3a67b2ecf31cc301dd639e0b4
matt windows <mathieu.muty@epitech.eu>
2025-09-30 21:58:44 +0200

    feat: get recipe steps

--- app/tools/scapper.py
+++ app/tools/scapper.py
@@ -27,6 +27,18 @@ def get_navbar_link() -> pd.DataFrame:
 def get_recipe_info():
     pass
 
+def get_data_from_recipe_url(route: str):
+    url = URL.rstrip("/")
+    print(f"url to fetch = {url + route}")
+    page = browser.get(f"{url + route}")
+    div = page.soup.find("div", class_="recipe-step-list")
+    if div:
+        text_content = div.get_text(strip=True, separator="\n")
+    else:
+        return "rien n'a été trouvé"
+    return text_content
+    
+
 def get_search_bar(search: str):
     main_page = browser.get(URL)
     main_page_html = main_page.soup
@@ -36,16 +48,17 @@ def get_search_bar(search: str):
     return result
 
 def main():
-    # df = get_navbar_link()
-    # print(df.head(5))
+    recipe_addr = []
     search_result = get_search_bar('sardine, pain')
-    # html = search_result.soup
     recipe_div = search_result.soup.find(id='content')
-    # print(recipe_div)
     links = recipe_div.find_all('a', href=True)
     for link in links:
         address = link['href']
         text = link.text.strip()
         print(f"{text} : {address}")
+        recipe_addr.append(address)
+    data = get_data_from_recipe_url(recipe_addr[0])
+    print(data)
+
 if __name__ == "__main__":
     main()
